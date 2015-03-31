e06d808fb4911ff141422e98f21b4d17c447e81b
Pegasus Wang <tianma201211@gmail.com>
2015-03-31 19:31:21 +0800

    change settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,7 +119,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wechat',    # my wechat app
-    'physics',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
